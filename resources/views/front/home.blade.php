cfa9298ac228d4be692fa49fd56a1cc28f12118a
nabilghi55 <nabilghi55@student.ub.ac.id>
2024-10-02 19:15:08 +0700

    hah

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,328 @@
+@extends('front.layouts.app')
+
+@section('content')
+<section class="section-1">
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel"
+        data-bs-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <picture>
+                    <source media="(min-width: 800px)" srcset="{{ asset('front-assets/images/carousel-1.jpg') }}" />
+                    <img src="{{ asset('front-assets/images/carousel-1.jpg') }}" alt="" />
+                </picture>
+
+                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                    <div class="p-3">
+                        <h1 class="display-4 text-white mb-3">DFP KEL 2</h1>
+                        <p class="mx-md-5 px-5">Beli hp baru atau second semua nya ready. Laptop bekas dan baru ready,
+                            dengan harga termurah se Indonesia</p>
+                        <a class="btn btn-outline-light py-2 px-4 mt-3" href="{{ route('front.shop') }}">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <picture>
+                    <source media="(max-width: 799px)" srcset="{{ asset('front-assets/images/carousel-2-m.jpg') }}" />
+                    <source media="(min-width: 800px)" srcset="{{ asset('front-assets/images/carousel-2.jpg') }}" />
+                    <img src="{{ asset('front-assets/images/carousel-2.jpg') }}" alt="" />
+                </picture>
+
+                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                    <div class="p-3">
+                        <h1 class="display-4 text-white mb-3">Womens Fashion</h1>
+                        <p class="mx-md-5 px-5">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet
+                            amet ndiam elitr ipsum diam</p>
+                        <a class="btn btn-outline-light py-2 px-4 mt-3" href="{{ route('front.shop') }}">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <picture>
+                    <source media="(max-width: 799px)" srcset="{{ asset('front-assets/images/carousel-3-m.jpg') }}" />
+                    <source media="(min-width: 800px)" srcset="{{ asset('front-assets/images/carousel-3.jpg') }}" />
+                    <img src="{{ asset('front-assets/images/carousel-3.jpg') }}" alt="" />
+                </picture>
+
+                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                    <div class="p-3">
+                        <h1 class="display-4 text-white mb-3">Shop Online at Flat 70% off on Branded Clothes</h1>
+                        <p class="mx-md-5 px-5">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet
+                            amet ndiam elitr ipsum diam</p>
+                        <a class="btn btn-outline-light py-2 px-4 mt-3" href="{{ route('front.shop') }}">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
+<section class="section-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="box shadow-lg">
+                    <div class="fa icon fa-check text-primary m-0 mr-3"></div>
+                    <h2 class="font-weight-semi-bold m-0">Quality Product</h2>
+                </div>
+            </div>
+            <div class="col-lg-3 ">
+                <div class="box shadow-lg">
+                    <div class="fa icon fa-shipping-fast text-primary m-0 mr-3"></div>
+                    <h2 class="font-weight-semi-bold m-0">Free Shipping</h2>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="box shadow-lg">
+                    <div class="fa icon fa-exchange-alt text-primary m-0 mr-3"></div>
+                    <h2 class="font-weight-semi-bold m-0">14-Day Return</h2>
+                </div>
+            </div>
+            <div class="col-lg-3 ">
+                <div class="box shadow-lg">
+                    <div class="fa icon fa-phone-volume text-primary m-0 mr-3"></div>
+                    <h2 class="font-weight-semi-bold m-0">24/7 Support</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="section-4 pt-5">
+    <div class="container">
+        <div class="section-title">
+            <h2>Featured Products</h2>
+        </div>
+        <div class="row pb-3">
+            @if ($featuredProducts->isNotEmpty())
+                @foreach ($featuredProducts as $product)
+                    <div class="col-md-3">
+                        <div class="card product-card">
+                            <div class="product-image position-relative" style="height: 250px;">
+                                <a href="{{ route("front.product", $product->slug) }}" class="product-img">
+
+                                    @if (!empty($product->product_image))
+                                        <img class="card-img-top {{ $product->qty == 0 ? 'out-of-stock' : '' }}"
+                                            src="{{ $product->product_image }}" />
+                                    @else
+                                        <img class="card-img-top {{ $product->qty == 0 ? 'out-of-stock' : '' }}"
+                                            src="{{ asset('admin-assets/img/default-150x150.png') }}" />
+                                    @endif
+
+                                </a>
+                                <a onclick="addToWishlist({{ $product->id }})" class="whishlist" href="javascript:void(0);"><i
+                                        class="far fa-heart"></i></a>
+                                <div class="product-action">
+                                    @if ($product->track_qty == 'Yes')
+                                        @if ($product->qty > 0)
+                                            <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                                                <i class="fa fa-shopping-cart"></i> Add To Cart
+                                            </a>
+                                        @else
+                                            <a class="btn btn-dark" href="javascript:void(0);" onclick="showOutOfStockAlert();">
+                                                Kehabisan Stock
+                                            </a>
+                                        @endif
+                                    @else
+                                        <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                                            <i class="fa fa-shopping-cart"></i> Add To Cart
+                                        </a>
+                                    @endif
+                                </div>
+
+                            </div>
+                            <div class="card-body text-center mt-3">
+                                <a class="h6 link" href="{{ route("front.product", $product->slug) }}">{{ $product->title }}</a>
+                                <div class="stock mt-2">
+                                    <small>{{ $product->qty > 0 ? 'Sisa Stok: ' . $product->qty : 'Out of Stock' }}</small>
+                                </div>
+                                <div class="price mt-2">
+                                    <span class="h5"><strong> <span class="rupiah">{{ $product->price }}</span></strong></span>
+                                    @if($product->compare_price > 0)
+                                        <span class="h6 text-underline"><del> <span
+                                                    class="rupiah">{{ $product->compare_price }}</span></del></span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+        </div>
+    </div>
+</section>
+
+<section class="section-4 pt-5">
+    <div class="container">
+        <div class="section-title">
+            <h2>Lastest Products</h2>
+        </div>
+        <div class="row pb-3">
+            @if ($latestProducts->isNotEmpty())
+                @foreach ($latestProducts as $product)
+                    <div class="col-md-3">
+                        <div class="card product-card">
+                            <div class="product-image position-relative" style="height: 250px;">
+                                <a href="{{ route("front.product", $product->slug) }}" class="product-img">
+
+                                    @if (!empty($product->product_image))
+                                        <img class="card-img-top {{ $product->qty == 0 ? 'out-of-stock' : '' }}"
+                                            src="{{ $product->product_image }}" />
+                                    @else
+                                        <img class="card-img-top {{ $product->qty == 0 ? 'out-of-stock' : '' }}"
+                                            src="{{ asset('admin-assets/img/default-150x150.png') }}" />
+                                    @endif
+
+                                </a>
+                                <a onclick="addToWishlist({{ $product->id }})" class="whishlist" href="javascript:void(0);"><i
+                                        class="far fa-heart"></i></a>
+                                <div class="product-action">
+                                    @if ($product->track_qty == 'Yes')
+                                        @if ($product->qty > 0)
+                                            <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                                                <i class="fa fa-shopping-cart"></i> Add To Cart
+                                            </a>
+                                        @else
+                                            <a class="btn btn-dark" href="javascript:void(0);" onclick="showOutOfStockAlert();">
+                                                Kehabisan Stock
+                                            </a>
+                                        @endif
+                                    @else
+                                        <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                                            <i class="fa fa-shopping-cart"></i> Add To Cart
+                                        </a>
+                                    @endif
+                                </div>
+
+                            </div>
+                            <div class="card-body text-center mt-3">
+                                <a class="h6 link" href="{{ route("front.product", $product->slug) }}">{{ $product->title }}</a>
+                                <div class="stock mt-2">
+                                    <small>{{ $product->qty > 0 ? 'In Stock: ' . $product->qty : 'Out of Stock' }}</small>
+                                </div>
+                                <div class="price mt-2">
+                                    <span class="h5"><strong> <span class="rupiah">{{ $product->price }}</span></strong></span>
+                                    @if($product->compare_price > 0)
+                                        <span class="h6 text-underline"><del> <span
+                                                    class="rupiah">{{ $product->compare_price }}</span></del></span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+        </div>
+    </div>
+</section>
+@endsection
+
+@section('customJs')
+<script>
+    function showOutOfStockAlert() {
+        Swal.fire({
+            icon: 'error',
+            title: 'Out of Stock',
+            text: 'Sorry, this product is currently out of stock.',
+            confirmButtonText: 'OK'
+        });
+    }
+
+    function addToCart(id) {
+        $.ajax({
+            url: '{{ route("front.addToCart") }}',
+            type: 'post',
+            data: { id: id },
+            dataType: 'json',
+            success: function (response) {
+                console.log(response);
+                if (response.status == true) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Added to Cart ',
+                        html: response.message,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'info',
+                        title: 'Product Already in Cart',
+                        html: response.message,
+                        confirmButtonText: 'OK',
+                        footer: '<a href="{{ route('front.cart') }}">Go to Cart</a>'
+                    });
+                }
+            }
+        });
+    }
+
+    function addToWishlist(id) {
+        $.ajax({
+            url: '{{ route("front.addToWishlist") }}',
+            type: 'post',
+            data: { id: id },
+            dataType: 'json',
+            success: function (response) {
+                console.log(response);
+                if (response.status == true) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Added to Wishlist',
+                        html: response.message,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'info',
+                        title: 'Product Already in Wishlist',
+                        html: response.message,
+                        confirmButtonText: 'OK',
+                        footer: '<a href="{{ route('account.wishlist') }}">Go to Wishlist</a>'
+                    });
+                }
+            }
+        });
+    }
+
+    // Function to format number as Rupiah
+    function formatRupiah(angka, prefix) {
+        var number_string = angka.toString().replace(/[^,\d]/g, '').toString(),
+            split = number_string.split(','),
+            sisa = split[0].length % 3,
+            rupiah = split[0].substr(0, sisa),
+            ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+        if (ribuan) {
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+        }
+
+        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+        return prefix == undefined ? rupiah : (rupiah ? 'Rp ' + rupiah : '');
+    }
+
+    // Apply the formatRupiah function to all elements with the class 'rupiah'
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelectorAll('.rupiah').forEach(function (element) {
+            element.textContent = formatRupiah(element.textContent, 'Rp');
+        });
+    });
+</script>
+
+<style>
+    .out-of-stock {
+        opacity: 0.5;
+    }
+</style>
+@endsection
